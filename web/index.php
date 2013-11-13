9d27f9e2f56553b359dcef926e6028e2169a0987
Rob Tyler <admin@obanmultilingual.com>
2013-11-13 12:32:51 +0000

    Removed PHP Fire from Master

--- web/index.php
+++ web/index.php
@@ -1,22 +1,6 @@
 <h1>sandbox.robtyler.dev.globalmaxer.com</h1>
 <?php
 
-// Load FirePHP so we an do a test console log
-require ( dirname(__FILE__) . '/3rdparty/FirePHPCore-0.3.2/lib/FirePHPCore/fb.php' );
-
-$objFirePHP	= FirePHP::getInstance(true);
-$objFirePHP->log ( 'Variable output goes here... for example...', 'FirePHP is working!' );
-$arrTest		= array ( 'i' => 10, 'j' => 20 );
-$objFirePHP->log ( $arrTest, 'Iterators' );
-// Send file and line information using a trace label
-$objFirePHP->trace ( 'Trace Label' );
-// Send different priority styles messages
-$objFirePHP->log ( 'Plain Message' );
-$objFirePHP->info ( 'Info Message' );
-$objFirePHP->warn ( 'Warn Message' );
-$objFirePHP->error ( 'Error Message' );  
-
-
 $arrDir = scandir ( '.' );
 echo '<ul>';
 
